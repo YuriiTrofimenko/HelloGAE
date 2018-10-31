b45379b6f1a9c956b070d42a2b8b1b36fb2a1d7f
YuriiTrofimenko <tyaa@ukr.net>
2018-10-31 22:33:20 +0200

    init

---
+++ src/main/java/org/tyaa/hellogae/HelloAppEngine.java
@@ -0,0 +1,30 @@
+package org.tyaa.hellogae;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+    
+    String x = request.getParameter("id");
+    
+    
+
+    response.getWriter().print("Hello App Engine!\r\n" + x);
+
+  }
+}
